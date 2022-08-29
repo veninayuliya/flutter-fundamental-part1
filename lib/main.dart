47fb74924bf54ba144b26b93efa68434b7684d09
veninayuliya <veninayuliya32@gmail.com>
2022-08-29 23:02:43 +0700

    4.5

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world/textfield_widget.dart';
 // import 'basic_widgets/loading_cupertino.dart';
-import 'basic_widgets/scaffold_widget.dart';
-
+// import 'basic_widgets/scaffold_widget.dart';
 // import 'basic_widgets/image_widget.dart';
 // import 'basic_widgets/text_widget.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MyTextField());
 }
