9d73b24975ed5b390d89b476435864a9d492fbce
veninayuliya <veninayuliya32@gmail.com>
2022-09-02 09:16:57 +0700

    update textfield

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:hello_world/basic_widgets/dialog_widget.dart';
+// import 'package:hello_world/basic_widgets/dialog_widget.dart';
+// import 'package:hello_world/basic_widgets/text_widget.dart';
+import 'package:hello_world/basic_widgets/textfield_widget.dart';
 // import 'package:hello_world/basic_widgets/datetime_widget.dart';
 // import 'package:hello_world/basic_widgets/scaffold_widget.dart';
-// import 'package:hello_world/textfield_widget.dart';
 // import 'basic_widgets/loading_cupertino.dart';
 // import 'basic_widgets/image_widget.dart';
-// import 'basic_widgets/text_widget.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: MyLayout(),
+        body: MyTextField(),
       ),
     );
   }
